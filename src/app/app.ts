2230200739c0fb24d57ad250b640196399059e44
DanielSet <Elessar.nazgul@gmail.com>
2025-10-20 15:38:15 +0300

    updated name

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('dnd-front');
+  protected readonly title = signal('angular-template');
 }
